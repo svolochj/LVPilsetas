7f410fb91ef7fe024fb217547c4b0c3fabfe6874
Vadims Sergejevs <vadims.sergejevs@gmail.com>
2022-06-05 12:42:17 +0000

    Maps options

--- app/webapp/controller/App.controller.js
+++ app/webapp/controller/App.controller.js
@@ -8,12 +8,27 @@ sap.ui.define([
         },
 
         onAfterHTMLRendering: function () {
-            console.log("map rendered");
+
+            const mapOptions = {
+                center:                     new google.maps.LatLng(57.15492695727807,23.370664978027307),
+                zoom:                       9,
+                zoomControl:                true,
+                zoomControlOptions:         {style: google.maps.ZoomControlStyle.DEFAULT},
+                disableDoubleClickZoom:     true,
+                mapTypeControl:             true,
+                mapTypeControlOptions:      {style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR},
+                scaleControl:               false,
+                scrollwheel:                true,
+                panControl:                 false,
+                streetViewControl:          true,
+                draggable :                 true,
+                overviewMapControl:         true,
+                overviewMapControlOptions:  {opened: false},
+                mapTypeId:                  google.maps.MapTypeId.ROADMAP,
+            };
+            
             const mapElement = document.getElementById("map");
-            const map = new google.maps.Map(mapElement, {
-                zoom: 4,
-                center: { lat: -25.344, lng: 131.031 }
-              });
+            const map = new google.maps.Map(mapElement, mapOptions);
         }
     })
 
