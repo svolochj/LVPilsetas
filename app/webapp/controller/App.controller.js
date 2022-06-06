db5ac1347b6f4903d3af1fc6d039e26a62d70f6d
Vadims Sergejevs <vadims.sergejevs@gmail.com>
2022-06-06 16:49:53 +0000

    Initial Pilsetas on the map

--- app/webapp/controller/App.controller.js
+++ app/webapp/controller/App.controller.js
@@ -4,20 +4,24 @@ sap.ui.define([
     'use strict';
     
     return Controller.extend("pilsetas.controller.App",{
+
+        map : {},
+
         onInit: function() {
+            this._readPilsetas();
         },
 
         onAfterHTMLRendering: function () {
 
             const mapOptions = {
-                center:                     new google.maps.LatLng(57.15492695727807,23.370664978027307),
+                center:                     new google.maps.LatLng(56.9,24.7),
                 zoom:                       9,
                 zoomControl:                true,
                 zoomControlOptions:         {style: google.maps.ZoomControlStyle.DEFAULT},
                 disableDoubleClickZoom:     true,
                 mapTypeControl:             true,
                 mapTypeControlOptions:      {style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR},
-                scaleControl:               false,
+                scaleControl:               true,
                 scrollwheel:                true,
                 panControl:                 false,
                 streetViewControl:          true,
@@ -28,8 +32,23 @@ sap.ui.define([
             };
             
             const mapElement = document.getElementById("map");
-            const map = new google.maps.Map(mapElement, mapOptions);
+            this.map = new google.maps.Map(mapElement, mapOptions);
+        },
+
+        _readPilsetas: function () {
+            $.get({url : "/api/pilsetas",success : (data) => {this._renderPilsetas(data)}, error : (error) => { /*Error handling*/ }});
+        },
+
+        _renderPilsetas: function (data) {
+            data.value.forEach(pilseta => {
+                var marker = new google.maps.Marker({
+                    position: 	new google.maps.LatLng(pilseta.coord_Lat, pilseta.coord_Long),
+                    map: 		this.map,
+                    title: 		pilseta.name,
+                });
+            });
         }
+
     })
 
 });
